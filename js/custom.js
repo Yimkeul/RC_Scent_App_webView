82f0d84e380cd57c76c9307533eb3cb98cfac8bd
YimKeul <43426556+YimKeul@users.noreply.github.com>
2022-01-14 21:31:11 +0900

    change jquery

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,6 @@ $(window).on('load',function(){
   $(".loadingtt").fadeOut(2000); // set duration in brackets    
 });
 
+// $(window).load(function(){
+//   $('.loadingtt').fadeOut(1000); // set duration in brackets    
+// });
