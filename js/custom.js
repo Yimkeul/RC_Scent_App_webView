6d4b7f9383ea8d5a727c2075abeba233ea6a8372
YimKeul <43426556+YimKeul@users.noreply.github.com>
2022-01-18 02:29:54 +0900

    upload play site

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,3 @@ $(window).on('load',function(){
   $(".loadingtt").fadeOut(2000); // set duration in brackets    
 });
 
-// $(window).load(function(){
-//   $('.loadingtt').fadeOut(1000); // set duration in brackets    
-// });
